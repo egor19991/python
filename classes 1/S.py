da35683a722cf250661863f2287a28f87aeac0fa
egor <egor@ekserver.local>
2023-02-27 15:51:48 +0700

    закончено занятие 1

--- classes 1/S.py
+++ classes 1/S.py
@@ -1,9 +1,13 @@
-#name = str(input())
-#price = int(input())
-#weight = int(input())
-#money = int(input())
-name="jfjdjfjjd"
+name = str(input())
+price = int(input())
+weight = int(input())
+money = int(input())
+sum = price * weight
+sur = money - sum
 print("================Чек================")
-print(f"Товар: {name:>34}" )
-print("fduкгПр".rjust(35))
-
+print("Товар:" + f"{name:>29}")
+print("Цена:" + f"{weight}кг * {price}руб/кг".rjust(30))
+print("Итого:" + f"{sum:>26}руб")
+print("Внесено:" + f"{money:>24}руб")
+print("Сдача:" + f"{sur:>26}руб")
+print("===================================")
---
+++ classes 1/T.py
@@ -0,0 +1,7 @@
+n = int(input())
+m = int(input())
+k1 = int(input())
+k2 = int(input())
+x2 = int((n * m - k1 * n) / (k2 - k1))
+x1 = int(n - x2)
+print(f"{x1} {x2}")
